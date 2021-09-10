c938d5daf15300b900e3070beb4eb42d10f3a6d2
Heidyrh13 <heidytati13@gmail.com>
2021-09-10 12:48:15 -0500

    Ejemplo de un componente en vue

---
+++ Semana3/p_vue1/appinicial/src/components/MiComponente.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Mi Componente </h1>
+    <p>
+        We offer you here some texts on various interesting subjects. You can practise reading comprehension and at the same time you will certainly learn something new.
+        Each text is identified with an icon showing the level of English used on it. You will also find some questions to answer at the end of some texts to test your comprehension.
+    </p>
+</template>
+
+<script>
+
+export default {};
+</script>
+
+<style>
+
+</style>
