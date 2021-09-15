8e1c22e338137df1f42f674caee246785f202679
Heidyrh13 <heidytati13@gmail.com>
2021-09-15 16:58:20 -0500

    Ejemplo calculadora con vue

---
+++ Semana3/p_vue1/calculadora/src/components/CalculadoraBasica.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  <h1>Calculadora Básica</h1>
+  <input type="number" v-model="n1" />
+  <input type="number" v-model="n2" />
+  <button @click="suma(n1,n2)">Sumar</button>
+  <button @click="resta(n1,n2)">Restar</button>
+  <br />
+  <span>{{ resultado }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "CalculadoraBasica",
+    data:() =>{
+        return {
+            n1:null,n2:null, resultado:""
+        };
+    },
+    methods: { 
+        suma(n1,n2) {
+            this.resultado = parseFloat(this.n1) + parseFloat(this.n2);
+        },
+        resta(n1,n2) {
+            this.resultado = parseFloat(this.n1) - parseFloat(this.n2);
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
