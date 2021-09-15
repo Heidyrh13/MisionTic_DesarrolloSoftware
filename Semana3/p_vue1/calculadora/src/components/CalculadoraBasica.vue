8e2137376193f3b7c4742483b9ff9a3d18098697
Heidyrh13 <heidytati13@gmail.com>
2021-09-15 18:34:14 -0500

    Se configura vuetify con vue en proyecto Calculadora

--- Semana3/p_vue1/calculadora/src/components/CalculadoraBasica.vue
+++ Semana3/p_vue1/calculadora/src/components/CalculadoraBasica.vue
@@ -1,10 +1,38 @@
 <template>
   <div>
   <h1>Calculadora Básica</h1>
-  <input type="number" v-model="n1" />
-  <input type="number" v-model="n2" />
-  <button @click="suma(n1,n2)">Sumar</button>
-  <button @click="resta(n1,n2)">Restar</button>
+
+  <v-text-field
+      type = "number"
+      label="Número 1"
+      :rules="rules"
+      hide-details="auto"
+      v-model="n1"
+    ></v-text-field>
+
+  <v-text-field
+      type = "number"
+      label="Número 2"
+      :rules="rules"
+      hide-details="auto"
+      v-model="n2"
+    ></v-text-field>
+
+  <v-btn
+    color="accent"
+    elevation="5"
+    rounded
+    text
+    @click="suma(n1,n2)"
+  >Sumar</v-btn>
+
+  <v-btn
+    color="accent"
+    elevation="5"
+    rounded
+    text
+    @click="resta(n1,n2)"
+  >Restar</v-btn>
   <br />
   <span>{{ resultado }}</span>
   </div>
@@ -15,7 +43,11 @@ export default {
     name: "CalculadoraBasica",
     data:() =>{
         return {
-            n1:null,n2:null, resultado:""
+            n1:null,n2:null, resultado:"",
+            rules: [
+                value => !!value || 'Requerido.',
+                value => (value && value.length >= 2) || 'Mínimo 2 caracteres',
+      ],
         };
     },
     methods: { 
